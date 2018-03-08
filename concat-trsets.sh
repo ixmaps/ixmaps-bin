3e9ebcb8103fabe95cceea363526f8a37406074d
Colin <colindmccann@gmail.com>
2018-03-08 16:41:01 +0200

    new script to create db stats via cronjob

--- concat-trsets.sh
+++ concat-trsets.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# This script concatenates all trsets into one giant trset
+
 echo > /var/www/ixmaps/trsets/00:_all_trsets.trset
 for filename in /var/www/ixmaps/trsets/*.trset; do
   if [[ "$filename" != "/var/www/ixmaps/trsets/00:_all_trsets.trset" ]]
---
+++ db-stats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script generates a small text file that is used by the website (db-stats.js for index.php)
+
+contributorsCount=$(psql ixmaps -t -c "SELECT COUNT (DISTINCT submitter) FROM traceroute")
+destinationsCount=$(psql ixmaps -t -c "SELECT COUNT (DISTINCT dest) FROM traceroute")
+traceroutesCount=$(psql ixmaps -t -c "SELECT COUNT (DISTINCT id) FROM traceroute")
+latestTraceroute=$(psql ixmaps -t -c "SELECT to_char(sub_time, 'DD Mon YYYY') FROM traceroute ORDER BY id DESC LIMIT 1;")
+
+json="var dbStats = {
+  'total_submitters': "$contributorsCount",
+  'total_destinations': "$destinationsCount",
+  'total_traceroutes': "$traceroutesCount",
+  'latest_contribution': '"$latestTraceroute"'
+}"
+
+echo $json > /var/www/ixmaps/_includes/db-stats.json
