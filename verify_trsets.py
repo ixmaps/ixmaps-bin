0767659836b1dba75cf2a30fb7dfa0d140412e36
Colin <colindmccann@gmail.com>
2020-07-21 17:19:50 -0400

    hybrid approach including ping for ip urls and get for standard urls

--- verify_trsets.py
+++ verify_trsets.py
@@ -3,12 +3,16 @@
 # This script confirms whether the trset URLs are all valid. It is triggered by a cronjob every night.
 # NB: remember to update the password as required on the server
 
+# We check urls that are only digits (eg an ip addr as the hostname) with a ping,
+# other urls with a GET (since non-terminating routes are still of value to us)
+
+import json
+import re
 import psycopg2
 import psycopg2.extras
 import requests
 from requests.exceptions import ConnectionError
-# import subprocess
-import json
+import subprocess
 
 def main():
   with open('/home/ixmaps/bin/config.json') as f:
@@ -25,9 +29,9 @@ def main():
   conn.close()
 
 
-# def ping(host):
-#   command = ['ping', '-c', '1', host]
-#   return subprocess.call(command) == 0
+def ping(host):
+  command = ['ping', '-c', '1', host]
+  return subprocess.call(command) == 0
 
 
 def verify(conn, cur):
@@ -37,24 +41,27 @@ def verify(conn, cur):
     url = row['url']
     print "Verifying: ", url
 
-    # if ping(url):
-    #   print "Reachable\n\n"
-    #   update_reachable(url, 'true', conn, cur)
-    # else:
-    #   print(url+" is unreachable...\n\n")
-    #   update_reachable(url, 'false', conn, cur)
-
-    # request requires very specific formatting (http://xyz.abc)
-    url = url.replace("http://", '')
-    url = url.replace("https://", '')
-    url = "http://" + url
-    try:
-      request = requests.get(url, timeout=10)
-      print('Reachable')
-      update_reachable(row['url'], 'true', conn, cur)
-    except requests.exceptions.RequestException as e:
-      print(url+' is unreachable...')
-      update_reachable(row['url'], 'false', conn, cur)
+    if bool(re.match('[\d/.]+$', url)):
+      print "PING"
+      if ping(url):
+        print "Reachable\n\n"
+        update_reachable(url, 'true', conn, cur)
+      else:
+        print(url+" is unreachable...\n\n")
+        update_reachable(url, 'false', conn, cur)
+    else:
+      print "GET"
+      # request requires very specific formatting (http://xyz.abc)
+      url = url.replace("http://", '')
+      url = url.replace("https://", '')
+      url = "http://" + url
+      try:
+        request = requests.get(url, timeout=10)
+        print('Reachable\n\n')
+        update_reachable(row['url'], 'true', conn, cur)
+      except requests.exceptions.RequestException as e:
+        print(url+' is unreachable...\n\n')
+        update_reachable(row['url'], 'false', conn, cur)
 
 
 def update_reachable(url, flag, conn, cur):
