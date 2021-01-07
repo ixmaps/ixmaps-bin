8b3eac689a45225142068be92714b74e4e3fda9c
Colin <colindmccann@gmail.com>
2021-01-07 18:37:37 -0500

    cannot move dir to itself

--- download_maxmind.sh
+++ download_maxmind.sh
@@ -8,7 +8,8 @@ MM_DATA_PATH="/home/ixmaps/ix-data/mm-data"
 echo "Downloading new version of MaxMind data on "$(date +%F)
 
 mkdir $MM_DATA_PATH/tmp
-mv $MM_DATA_PATH/* $MM_DATA_PATH/tmp/
+mv $MM_DATA_PATH/GeoLite2-ASN.mmdb $MM_DATA_PATH/tmp/
+mv $MM_DATA_PATH/GeoLite2-City.mmdb $MM_DATA_PATH/tmp/
 
 asn_zip=$(wget -O $MM_DATA_PATH/GeoLite2-ASN.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=$MM_LICENSE_KEY&suffix=tar.gz")
 asn_download_status=$?
